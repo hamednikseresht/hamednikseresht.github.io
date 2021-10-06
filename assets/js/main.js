76dd657e9841a95ba135327aabd9aa6e1ea7bf77
Ho3ein <hos.kh93@gmail.com>
2021-10-06 17:22:59 +0330

    adjust mobile menu for index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,4 +31,34 @@
 			offset: function() { return $nav.height(); }
 		});
 
+		$('#mobile_nav a, .scrolly').scrolly({
+			speed: 1000,
+			offset: function() { return $nav.height(); }
+		});
+
 })(jQuery);
+
+// variables
+const mobile_menu_button = document.querySelector("#mobile_menu_button");
+const mobile_menu = document.querySelector("#mobile_menu")
+
+// ready whole document
+document.addEventListener("DOMContentLoaded" , ()=>{
+	mobile_menu_button.addEventListener("click" , mobile_menu_button_click);
+});
+
+// check if menu open and cursor clicked outside hide menu
+document.addEventListener('click' , (e)=>{
+	if(
+		(! mobile_menu.classList.contains("toggle_menu")) &&
+		(! e.target.classList.contains("mobile_menu_item_link")) && 
+		(e.target.id != "mobile_menu_button")
+	){
+		mobile_menu.classList.add("toggle_menu")	
+	}
+});	
+
+// functions
+const mobile_menu_button_click = (e) =>{
+	mobile_menu.classList.toggle("toggle_menu");
+}
